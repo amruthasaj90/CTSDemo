e35b8b1aba380b03f2bd5908bbeb844b0654519d
pgokul789 <gokul.p@inspirisys.com>
2020-03-12 13:08:41 +0530

    Added UI to for listing Facts

--- app/src/main/java/com/cts/demo/viewholder/FactsListViewHolder.kt
+++ app/src/main/java/com/cts/demo/viewholder/FactsListViewHolder.kt
@@ -1,6 +1,11 @@
 package com.cts.demo.viewholder
 
 import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
+import com.cts.demo.R
 import com.cts.demo.databinding.InflateFactsListItemBinding
 import com.cts.demo.listener.BaseRecyclerAdapterListener
 import com.cts.demo.model.response.Facts
@@ -13,7 +18,18 @@ class FactsListViewHolder(
 
 
     override fun populateData(data: Facts) {
+        viewDataBinding.factsData = data
+        viewDataBinding.listener = listener
 
+        if(!data.imageHref.isNullOrEmpty()) {
+            val requestOptions = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .override(300, 300)
+            Glide.with(mContext)
+                .load(data.imageHref)
+                .thumbnail(0.5f).apply(requestOptions)
+                .into(viewDataBinding.ivFacts)
+        }
     }
 
 }
